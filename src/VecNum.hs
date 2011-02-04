e8e36eddd934d28a8ff2188145369e7b8d0006a6
Conal Elliott <conal@conal.net>
2011-02-03 23:01:42 -0800

    VecNum

--- src/VecNum.hs
+++ src/VecNum.hs
@@ -1,8 +1,20 @@
 {-# LANGUAGE CPP, TypeOperators, FlexibleContexts #-}
 {-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
 
--- Num instances for `Vec n`
+----------------------------------------------------------------------
+-- |
+-- Module      :  VecNum
+-- Copyright   :  (c) Conal Elliott 2011
+-- License     :  BSD3
+-- 
+-- Maintainer  :  conal@conal.net
+-- Stability   :  experimental
+-- 
+-- Num instances for @Vec n@ and @f :^ n@.
 -- Perform numeric operations elementwise.
+-- 
+----------------------------------------------------------------------
+
 
 module VecNum where
 
@@ -11,6 +23,7 @@ import Data.Foldable (Foldable)
 
 import Nat
 import Vec
+import ShowF
 import ComposeFunctor
 
 -- Generate bogus (error-producing) Enum
@@ -25,8 +38,8 @@ import ComposeFunctor
 #undef APPLICATIVE
 
 -- Generate bogus (error-producing) Show. TODO: real ones.
-#define INSTANCE_Show
+-- #define INSTANCE_Show
 
-#define CONSTRAINTS IsNat n, Applicative f, Foldable f,
+#define CONSTRAINTS IsNat n, Applicative f, Foldable f, ShowF f,
 #define APPLICATIVE (f :^ n)
 #include "ApplicativeNumeric-inc.hs"
