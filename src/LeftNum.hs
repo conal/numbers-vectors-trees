4e8341e3f8ef69e209aedddb1e9bffd7e218a021
Conal Elliott <conal@conal.net>
2011-02-03 22:59:17 -0800

    left Vec & f:^n as numeric

--- src/LeftNum.hs
+++ src/LeftNum.hs
@@ -1,6 +1,21 @@
 {-# LANGUAGE CPP, TypeOperators #-}
 {-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
 
+----------------------------------------------------------------------
+-- |
+-- Module      :  LeftNum
+-- Copyright   :  (c) Conal Elliott 2011
+-- License     :  BSD3
+-- 
+-- Maintainer  :  conal@conal.net
+-- Stability   :  experimental
+-- 
+-- Num instances for @Vec n@ and @f :^ n@.
+-- Perform numeric operations elementwise.
+--
+-- To do: Eliminate this module, moving result into Left.hs
+-----------------------------------------------------------
+
 -- Num instances for left-folding versions of `Vec n` and `f :^ n`
 -- Perform numeric operations elementwise.
 
@@ -23,7 +38,7 @@ import Left
 #undef APPLICATIVE
 
 -- Generate bogus (error-producing) Show. TODO: real ones.
-#define INSTANCE_Show
+-- #define INSTANCE_Show
 
 #define CONSTRAINTS IsNat n, Applicative f, Foldable f,
 #define APPLICATIVE (f :^ n)
